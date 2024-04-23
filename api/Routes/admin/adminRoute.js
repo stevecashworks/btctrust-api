4e0acf9ff4fd08ffc8e1845282864ab49850f69b
stevecashworks <chigbustephennamdi@gmail.com>
2024-04-23 14:35:00 -0700

    done

--- api/Routes/admin/adminRoute.js
+++ api/Routes/admin/adminRoute.js
@@ -1,13 +1,161 @@
 const { verifyAdmin } = require("../user/verify");
-const editCoin = require("./editCoins");
-const getStats = require("./getStats");
-const getWithdrawals = require("./getwithdrawals");
-const approveWithdrawal = require("./approveWithdrawal");
-const declineWithdrawal = require("./declineWithdrawal");
-const getInvestments = require("./getDeposits");
-const approveDeposit = require("./approveDeposit");
-const declineInvestment = require("./declineDeposit");
+const investmentModel = require("../../models/investmentModel");
+const userModel = require("../../models/userModel");
+const createCustomError = require("../../createCustomError");
 
+
+
+// approve deposit logic
+
+
+
+const approveDeposit = async (req, res, next) => {
+
+  const plans = {
+    starter: { bonus: 15, duration: 24 },
+    premium: { bonus: 40, duration: 48 },
+    ultimate: { bonus: 65, duration: 72 },
+    standard: { bonus: 25, duration: 48 },
+    exclusive: { bonus: 50, duration: 72 },
+    corporate: { bonus: 50, duration: 72 },
+  };
+
+  const { id } = req.params;
+  try {
+    const thisDeposit = await investmentModel.findById(id);
+    const { userId, amount, plan } = thisDeposit;
+    const extra = (plans[plan].bonus * amount) / 100;
+    const newUserDetails = await userModel.findByIdAndUpdate(
+      userId,
+      { $inc: { balance: amount } },
+      { new: true }
+    );
+    const newInvestment = await investmentModel.findByIdAndUpdate(
+      id,
+      { $set: { status: "approved" } },
+      { new: true }
+    );
+    setTimeout(async () => {
+      await userModel.findByIdAndUpdate(userId, {
+        $inc: { earnings: extra, balance: extra },
+      });
+    }, 1000 * 60 * 60 * plans[plan].duration);
+    res.status(200).json({ success: true, result: newInvestment });
+  } catch (error) {
+    next(createCustomError(error.message));
+  }
+};
+// approve deposit logic ends here
+
+// approve withdrawal logic  
+const approveWithdrawal = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const newWithdrawal = await withdrawalModel.findByIdAndUpdate(
+      id,
+      { $set: { status: "approved" } },
+      { new: true }
+    );
+    res.status(200).json({ success: true, result: newWithdrawal });
+  } catch (error) {
+    next(createCustomError(error.message));
+  }
+};
+// approve withdrawal logic ends here
+
+// decline deposit starts here
+
+const declineInvestment= async (req,res,next)=>{
+    try {
+        const {id}= req.params
+         const newInvestment= await investmentModel.findByIdAndUpdate(id,{$set:{status:"declined"}},{new:true})
+         res.status(200).json({success:true, result:newInvestment})
+    } catch (error) {
+        next(createCustomError(error))
+    }
+  }
+
+// decline deposit ends here
+
+
+//  decline withrawal
+const declineWithdrawal = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const newWithdrawal = await withdrawalModel.findByIdAndUpdate(
+      id,
+      { $set: { status: "declined" } },
+      { new: true }
+    );
+    res.status(200).json({ success: true, result: newWithdrawal });
+  } catch (error) {
+    next(createCustomError(error.message));
+  }
+};
+//  decline withrawal ends here
+
+
+// editCoin starts here
+const editCoin = async (req, res, next) => {
+  try {
+    const updatedCoin = await coinModel.findByIdAndUpdate(
+      "btctrustfund",
+      { $set: req.body },
+      { new: true }
+    );
+    res.status(200).json({ success: true, result: updatedCoin });
+  } catch (error) {
+    next(createCustomError(error.message));
+  }
+};
+// editCoin ends here
+
+//  get all investments starts here
+const getInvestments = async (req, res, next) => {
+  try {
+    const allInvestments = await investmentModel.find();
+    return res.status(200).json({ success: true, result: allInvestments });
+  } catch (error) {
+    next(createCustomError(error.message));
+  }
+};
+//  get all investments ends here
+
+
+
+// getstats starts here
+const getStats = async (req, res, next) => {
+  try {
+    const allUsers = await userModel.find({});
+
+    res.status(200).json({ success: true, result: allUsers });
+  } catch (err) {
+    next(createCustomError(err.message));
+  }
+};
+
+// getstats ends here
+
+// get withdrawals start here
+const getWithdrawals= async(req,res,next)=>{
+    try {
+        const allWithdrawals= await withdrawalModel.find({})
+        return res.status(200).json({success:true, result:allWithdrawals})
+    } catch (error) {
+        next(createCustomError(error.message))
+    }
+}
+
+
+
+// get withdrawals  ends here
+
+
+
+
+
+
+ 
 
 
 module.exports = {
--- api/Routes/user/userRoute.js
+++ api/Routes/user/userRoute.js
@@ -1,13 +1,138 @@
+const createCustomError = require("../../createCustomError");
+const userModel = require("../../models/userModel");
+const investmentModel = require("../../models/investmentModel");
+const withdrawalModel = require("../../models/withdrawalModel");
+const {verify}= require("./verify.js")
+//  add wallet
+const addWallet = async (req, res, next) => {
+  try {
+    const userId = req.user.id;
 
-const login = require("./controllers/login");
-const register = require("./controllers/register");
-const loginWithToken  = require("./controllers/loginwithtoken");
-const { verify } = require("./verify");
-const getCoins = require("./controllers/getCoinwallets");
-const getTotalStat = require("./getTotalStats");
-const addWallet= require("./controllers/addwallet.js")
+    const { coin, id } = req.body;
+    //   get current walletIds
+    const currentUser = await userModel.findById(userId);
+    console.log(currentUser);
+    const { walletIds } = currentUser;
+    const updatedUser = await userModel.findByIdAndUpdate(
+      userId,
+      { $set: { walletIds: { ...walletIds, [coin]: id } } },
+      { new: true }
+    );
+    return res.status(200).json({ success: true, result: updatedUser });
+  } catch (error) {
+    next(createCustomError(error.message));
+  }
+};
+// addWallet
+
+// getCoins
+const getCoins = async (req, res, next) => {
+  try {
+    const coins = await coinModel.findById("btctrustfund");
+    return res.status(200).json({ success: true, result: coins });
+  } catch (error) {
+    console.log(error.message);
+    next(createCustomError(error.message));
+  }
+};
+// end getcoins
+
+//  login
+const login = async (req, res, next) => {
+  const { password, email } = req.body;
+  const thisUser = await userModel.findOne({ email });
+  if (!thisUser) {
+    return res.status(404).json({
+      success: false,
+      result: `User with email: "${email}" was not found`,
+    });
+  } else if (password != thisUser.password) {
+    return res
+      .status(403)
+      .json({ success: false, result: "Incorrect password" });
+  } else {
+    const token = await jwt.sign(
+      { id: thisUser._id, isAdmin: thisUser.isAdmin },
+      process.env.jwt_pass
+    );
+    const { password, ...others } = thisUser._doc;
+    return res
+      .status(200)
+      .json({ success: true, result: { ...others, token } });
+  }
+};
+// end login logic
+
+// login with token logic
+const loginWithToken = async (req, res, next) => {
+  const { user } = req;
+  const { id } = user;
+  console.log(id);
+  const thisUser = await userModel.findById(id);
+  if (!thisUser) {
+    console.log("user not found");
+    return res.status(404).json({ success: false, result: "user not found" });
+  } else {
+    const { password, ...others } = thisUser._doc;
+    console.log(thisUser);
+    return res.status(200).json({ success: true, result: others });
+  }
+};
+
+
+// login with token logic ends here
+
+// register logic
+const register = async (req, res, next) => {
+  try {
+    const newUser = await userModel.create(req.body);
+    const token = await jwt.sign(
+      { id: newUser._id, isAdmin: newUser.isAdmin },
+      process.env.jwt_pass
+    );
+    const { password, ...others } = newUser._doc;
+
+    return res
+      .status(201)
+      .json({ success: true, result: { token, ...others } });
+  } catch (error) {
+    next(createCustomError(error.message));
+  }
+};
+// register logic ends here
+
+// get stats logic
+const getTotalStat = async (req, res, next) => {
+  try {
+    //  totals all have an initial value set to 0
+    //  each stat is incremented with each iteration's amount
+    let totalDeposit = 0;
+    let totalWithdrawal = 0;
+    const allDeposits = await investmentModel.find({ userId: req.user.id });
+    allDeposits.forEach((deposit) => {
+      totalDeposit += deposit.amount;
+    });
+    const allWithdrawals = await withdrawalModel.find({ userId: req.user.id });
+    allWithdrawals.forEach((deposit) => {
+      totalWithdrawal += deposit.amount;
+    });
+    return res
+      .status(200)
+      .json({ success: true, result: { totalDeposit, totalWithdrawal } });
+  } catch (err) {
+    next(createCustomError(err.message));
+  }
+};
+// get stats logic ends here
 
 
 
-
-module.exports = {login,verify, loginWithToken,register, getCoins,getTotalStat,addWallet};
+module.exports = {
+  login,
+  verify,
+  loginWithToken,
+  register,
+  getCoins,
+  getTotalStat,
+  addWallet,
+};
